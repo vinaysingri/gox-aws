255b82cfc797e1ef7982a83047b55668dd470b0b
Harish Bohara <you@example.com>
2021-05-10 00:47:00 +0530

    added test o- verify: 1. bad schema does not work; 2. old client will be able to parse data from new schema

--- schema/avro/interface_test.go
+++ schema/avro/interface_test.go
@@ -54,3 +54,79 @@ func TestMockAvroEngine(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, input["data"], output["data"])
 }
+
+func TestAvroEngine_Versioning(t *testing.T) {
+
+	schema := `
+		{
+		   "type" : "record",
+		   "namespace" : "gox.aws",
+		   "name" : "Employee",
+		   "fields" : [
+			  { "name" : "Name" , "type" : "string" },
+			  { "name" : "Age" , "type" : "int" }
+		   ]
+		}`
+
+	// Test 1 - Schema with V1
+	engine, err := NewAvroSchemaEngine(schema)
+	assert.NoError(t, err)
+
+	// Make sure we are able to convert to avro and back to object
+	input := map[string]interface{}{"Name": "user_1", "Age": 10}
+	data, err := engine.ToAvro(input)
+	assert.NoError(t, err)
+	backObject, err := engine.FromAvro(data)
+	assert.NoError(t, err)
+	assert.Equal(t, "user_1", backObject.StringOrEmpty("Name"))
+	assert.Equal(t, 10, backObject.IntOrDefault("Age", 0))
+
+	// Test 2 - Schema with V2 with a BAD schema change
+	schemaWithBadChanges := `
+		{
+		   "type" : "record",
+		   "namespace" : "gox.aws",
+		   "name" : "Employee",
+		   "fields" : [
+			  { "name" : "Name" , "type" : "string" },
+			  { "name" : "Age" , "type" : "string" }
+		   ]
+		}`
+	engineWithBadSchema, err := NewAvroSchemaEngine(schemaWithBadChanges)
+	assert.NoError(t, err)
+	backObject, err = engineWithBadSchema.FromAvro(data)
+	assert.Error(t, err)
+
+	// Test 3 - Schema with V3 with new attribute added
+	schemaWithNewAttribute := `
+		{
+		   "type" : "record",
+		   "namespace" : "gox.aws",
+		   "name" : "Employee",
+		   "fields" : [
+			  { "name" : "Name" , "type" : "string" },
+			  { "name" : "Age" , "type" : "int" },
+              { "name" : "Dob" , "type" : "string" }
+		   ]
+		}`
+	input = map[string]interface{}{"Name": "user_1", "Age": 10, "Dob": "1700"}
+	enginesWithNewAttribute, err := NewAvroSchemaEngine(schemaWithNewAttribute)
+	assert.NoError(t, err)
+	dataWithDob, err := enginesWithNewAttribute.ToAvro(input)
+	assert.NoError(t, err)
+	backObject, err = enginesWithNewAttribute.FromAvro(dataWithDob)
+	assert.NoError(t, err)
+	assert.Equal(t, "user_1", backObject.StringOrEmpty("Name"))
+	assert.Equal(t, 10, backObject.IntOrDefault("Age", 0))
+	assert.Equal(t, "1700", backObject.StringOrEmpty("Dob"))
+
+	// Test 4 - See if the client with Old schema can work with the new data or now
+	backObject, err = engine.FromAvro(dataWithDob)
+	assert.NoError(t, err)
+	assert.Equal(t, "user_1", backObject.StringOrEmpty("Name"))
+	assert.Equal(t, 10, backObject.IntOrDefault("Age", 0))
+
+	// Test 4.1 - since we are using "engine" from old schema it should work with new data
+	// but it should not see the new values
+	assert.Equal(t, "", backObject.StringOrEmpty("Dob"))
+}

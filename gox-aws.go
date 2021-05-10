c0c3e6bbb83cfff4f2e1a6801fcc6a8dd2d9d962
Harish Bohara <you@example.com>
2021-05-10 10:57:12 +0530

    added aws session without endpoint

--- gox-aws.go
+++ gox-aws.go
@@ -20,11 +20,18 @@ func NewAwsContext(config Config) (ctx AwsContext, err error) {
 	_ctx := &awsSessionContext{CrossFunction: gox.NewNoOpCrossFunction()}
 
 	// Setup AWS session
-	_ctx.session, err = session.NewSession(
-		&aws.Config{
-			Region:   aws.String(config.Region),
-			Endpoint: aws.String(config.Endpoint),
-		})
+	if len(config.Endpoint) > 0 {
+		_ctx.session, err = session.NewSession(
+			&aws.Config{
+				Region:   aws.String(config.Region),
+				Endpoint: aws.String(config.Endpoint),
+			})
+	} else {
+		_ctx.session, err = session.NewSession(
+			&aws.Config{
+				Region: aws.String(config.Region),
+			})
+	}
 	if err != nil {
 		return nil, errors2.Wrap(err, "failed to create aws session: endpoint=%s, region=%s", config.Endpoint, config.Region)
 	}

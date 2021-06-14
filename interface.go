9301b5e3b3a0d9df2bda82ed408b4843e75a1b13
Harish Bohara <you@example.com>
2021-06-14 15:04:35 +0530

    added key info is AWS config

--- interface.go
+++ interface.go
@@ -4,8 +4,11 @@ import "github.com/aws/aws-sdk-go/aws/session"
 
 // Dynamo DB setup configuration
 type Config struct {
-	Region   string `json:"region,string" yaml:"region,string"`
-	Endpoint string `json:"endpoint,string" yaml:"endpoint,string"`
+	Region        string `json:"region,string" yaml:"region,string"`
+	Endpoint      string `json:"endpoint,string" yaml:"endpoint,string"`
+	AwsAccessKey  string `json:"aws_access_key,string" yaml:"aws_access_key,string"`
+	AwsSecretKey  string `json:"aws_secret_key,string" yaml:"aws_secret_key,string"`
+	AwsSessionKey string `json:"session_token,string" yaml:"session_token,string"`
 }
 
 type AwsContext interface {

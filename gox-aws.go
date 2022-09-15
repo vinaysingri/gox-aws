53781d98d2e2f4c706995bc6646d8ab40995cb3e
vinay singri <vinay.singri@gmail.com>
2022-09-15 11:42:28 +0530

    Update gox-aws.go

--- gox-aws.go
+++ gox-aws.go
@@ -2,6 +2,7 @@ package gox_aws
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/devlibx/gox-base"
 	errors2 "github.com/devlibx/gox-base/errors"
@@ -19,25 +20,38 @@ func (a *awsSessionContext) GetSession() *session.Session {
 
 func NewAwsContext(cf gox.CrossFunction, config Config) (ctx AwsContext, err error) {
 	_ctx := &awsSessionContext{CrossFunction: cf}
+	var region *string = nil
+	var endpoint *string = nil
+	var creds *credentials.Credentials = nil
 
 	// Set default region
 	if util.IsStringEmpty(config.Region) {
 		config.Region = "ap-south-1"
+	} else {
+		region = aws.String(config.Region)
 	}
 
-	// Setup AWS session
+	// use end point if configured
 	if len(config.Endpoint) > 0 {
-		_ctx.session, err = session.NewSession(
-			&aws.Config{
-				Region:   aws.String(config.Region),
-				Endpoint: aws.String(config.Endpoint),
-			})
-	} else {
-		_ctx.session, err = session.NewSession(
-			&aws.Config{
-				Region: aws.String(config.Region),
-			})
+		endpoint = aws.String(config.Endpoint)
 	}
+
+	// use credentials if configured
+	if !util.IsStringEmpty(config.AwsAccessKey) {
+		creds = credentials.NewStaticCredentials(
+			config.AwsAccessKey,
+			config.AwsSecretKey,
+			config.AwsSessionKey)
+
+	}
+
+	_ctx.session, err = session.NewSession(
+		&aws.Config{
+			Credentials: creds,
+			Region:      region,
+			Endpoint:    endpoint,
+		})
+
 	if err != nil {
 		return nil, errors2.Wrap(err, "failed to create aws session: endpoint=%s, region=%s", config.Endpoint, config.Region)
 	}
